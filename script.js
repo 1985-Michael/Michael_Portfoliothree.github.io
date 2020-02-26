2d970a6b991a36ef2ef80f51050af6c8312d7f59
Michael Myers <anthonymyers1985@gmail.com>
2020-02-26 15:38:20 -0800

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+function newTab1() {
+    window.open(
+        "https://www.linkedin.com/in/michael-anthony-myers-34115bab/", "_blank");
+}
+
+function newTab2() {
+    window.open(
+        "https://github.com/1985-Michael", "_blank");
+}
